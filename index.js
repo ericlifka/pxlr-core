abd1149866be81924c60ffbf465db944b2a922ba
Eric Lifka <eric.lifka@gmail.com>
2016-04-11 21:08:20 -0400

    bring over the base set of core utilities, still torn on game-object and frame, they're too phoenix focused for now

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ DefineModule('pxlr/core', function () {
 });
 
 /* provide namespace backwards compatibility for v1 */
+DefineModule('models/animation', function (require) {
+    return require('pxlr/core/animation');
+});
 DefineModule('models/cell-grid', function (require) {
     return require('pxlr/core/cell-grid');
+});
+DefineModule('models/sprite', function (require) {
+    return require('pxlr/core/sprite');
+});
+DefineModule('models/sprite-group', function (require) {
+    return require('pxlr/core/sprite-group');
 });
